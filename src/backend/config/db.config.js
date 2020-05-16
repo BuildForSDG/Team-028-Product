02b2fc07f9ab9f6524d22a2dd7fedb4cd24e4f27
Onyedikachi <onyedikachinwosu@rocketmail.com>
2020-05-16 14:36:30 +0100

    feat : update model relationships

--- src/backend/config/db.config.js
+++ src/backend/config/db.config.js
@@ -1,6 +1,6 @@
-const env = require("./env.js");
+const Sequelize = require('sequelize');
+const env = require('./env.js');
 
-const Sequelize = require("sequelize");
 const sequelize = new Sequelize(env.database, env.username, env.password, {
   host: env.host,
   dialect: env.dialect,
@@ -10,8 +10,8 @@ const sequelize = new Sequelize(env.database, env.username, env.password, {
     max: env.max,
     min: env.pool.min,
     acquire: env.pool.acquire,
-    idle: env.pool.idle,
-  },
+    idle: env.pool.idle
+  }
 });
 
 const db = {};
@@ -19,14 +19,26 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-//Models/tables
-db.users = require("../model/user.model.js")(sequelize, Sequelize);
-db.userCategory = require("../model/usercategory.model")(sequelize, Sequelize);
-db.userOrganization = require("../model/organization.model")(
+// Models/tables
+db.users = require('../model/user.model.js')(sequelize, Sequelize);
+db.userCategory = require('../model/usercategory.model')(sequelize, Sequelize);
+db.organizations = require('../model/organization.model')(
   sequelize,
   Sequelize
 );
-db.userPrevilege = require("../model/previleges.model")(sequelize, Sequelize);
-db.userLogin = require("../model/userLoggin.model")(sequelize, Sequelize);
+db.privileges = require('../model/privileges.model')(sequelize, Sequelize);
+db.userLogin = require('../model/userLoggin.model')(sequelize, Sequelize);
+db.roles = require('../model/role.model')(sequelize, Sequelize);
+db.roleprivileges = require('../model/roleprivileges.model')(sequelize, Sequelize);
+db.audits = require('../model/audit.model')(sequelize, Sequelize);
+
+
+db.users.belongsTo(db.organizations, { foreignKey: 'organizationId', as: 'organizations' });
+db.organizations.hasMany(db.users, { as: 'users' });
+
+db.roles.belongsToMany(db.privileges, { through: 'roleprivileges', foreignKey: 'roleId', as: 'privileges' });
+db.roleprivileges.belongsTo(db.roles, { foreignKey: 'roleId' });
+db.roleprivileges.belongsTo(db.privileges, { foreignKey: 'privilegeId' });
+db.roles.belongsToMany(db.roles, { through: 'roleprivileges', foreignKey: 'privilegeId', as: 'roles' });
 
 module.exports = db;
